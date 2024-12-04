667957e7a3203bc470ea8875eef18b7980002f1c
CptNoodlz <techadminoo@63q8gc.onmicrosoft.com>
2024-12-04 13:45:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.95"
-    }
-    azapi = {
-      source  = "azure/azapi"
-      version = "~> 1.12"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
-  }
-}
-
 module "avm-res-resources-resourcegroup" {
   source   = "Azure/avm-res-resources-resourcegroup/azurerm"
   version  = "0.1.0"
