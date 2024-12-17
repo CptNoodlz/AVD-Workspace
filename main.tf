8f0023771ab49ba2cf9dd46c36bc3057d5c622b1
CptNoodlz <techadminoo@63q8gc.onmicrosoft.com>
2024-12-17 14:24:33 -0500

    ADD VIRTUAL NETTWORK (#19)

--- main.tf
+++ main.tf
@@ -4,3 +4,19 @@ module "avm-res-resources-resourcegroup" {
   location = "eastus2"
   name     = "Testrg1"
 }
+
+module "avm-res-network-virtualnetwork" {
+  source              = "Azure/avm-res-network-virtualnetwork/azurerm"
+  version             = "0.7.1"
+  address_space       = ["10.0.20.0" / 19]
+  location            = "East US2"
+  name                = "TestVnet"
+  resource_group_name = "Testrg1"
+}
+/*#module "avm-res-sql-managedinstance" {
+  source  = "Azure/avm-res-sql-managedinstance/azurerm"
+  version = "0.1.0"
+  location = "eastus2"
+  name = "TestMI"
+  resource_group_name = "Testrg1"
+  }*/
