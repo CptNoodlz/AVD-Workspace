342235f0956d24d14cf4d0a18d0bc48420802786
Ozan <463721@wegmans.com>
2025-05-05 14:09:44 -0400

    stagedch

--- main.tf
+++ main.tf
@@ -5,29 +5,6 @@ module "avm-res-resources-resourcegroup" {
   name     = "Testrg1"
 }
 
-module "avm-res-network-virtualnetwork" {
-  source              = "Azure/avm-res-network-virtualnetwork/azurerm"
-  version             = "0.8.1"
-  address_space       = ["10.0.0.0/19"]
-  location            = var.location
-  name                = "TestVnet1"
-  resource_group_name = "Testrg1"
-}
-module "avm-res-sql-managedinstance" {
-  source                       = "Azure/avm-res-sql-managedinstance/azurerm"
-  version                      = "0.1.0"
-  location                     = var.location
-  name                         = "testmi"
-  resource_group_name          = "Testrg1"
-  subnet_id                    = "/subscriptions/4bcc135f-80a8-482f-b67b-fc849aa4b7ac/resourceGroups/Testrg1/providers/Microsoft.Network/virtualNetworks/TestVnet1/subnets/default"
-  storage_size_in_gb           = "512"
-  sku_name                     = "GP_Gen5"
-  license_type                 = "LicenseIncluded"
-  vcores                       = "4"
-  administrator_login          = "techadmin"
-  administrator_login_password = "PZH24X$3mE!!"
-}
-
 module "avm-res-maintenance-maintenanceconfiguration" {
   source              = "Azure/avm-res-maintenance-maintenanceconfiguration/azurerm"
   version             = "0.1.0"
--- providers.tf
+++ providers.tf
@@ -25,7 +25,6 @@ terraform {
 
 provider "azurerm" {
   subscription_id            = "4bcc135f-80a8-482f-b67b-fc849aa4b7ac"
-  skip_provider_registration = false
   features {}
 }
 
