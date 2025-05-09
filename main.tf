c5dacc247a883568767f69483a485e1d923e6947
CptNoodlz <techadminoo@63q8gc.onmicrosoft.com>
2025-05-09 10:04:19 -0400

    changeroni (#46)
    Co-authored-by: Ozan <463721@wegmans.com>

--- main.tf
+++ main.tf
@@ -32,3 +32,30 @@ module "avm-res-maintenance-maintenanceconfiguration" {
   }
 }
 
+module "avm-res-maintenance-maintenanceconfiguration" {
+  source              = "Azure/avm-res-maintenance-maintenanceconfiguration/azurerm"
+  version             = "0.1.0"
+  location            = var.location
+  name                = "TestConfig2"
+  resource_group_name = "Testrg1"
+  scope               = "InGuestPatch"
+  extension_properties = {
+    InGuestPatchMode = "User"
+  }
+  install_patches = {
+    linux = {
+      classifications_to_include = ["Critical", "Security"]
+    }
+    reboot_setting = "IfRequired"
+    windows = {
+      classifications_to_include = ["Critical", "Security"]
+    }
+  }
+  window = {
+    time_zone       = "Eastern Standard Time"
+    recur_every     = "7Day"
+    start_date_time = "2025-05-04 00:00"
+    duration        = "2:00"
+  }
+}
+
