b399486aaaceab1b5c81c86fb2d2fd4632d417d4
CptNoodlz <techadminoo@63q8gc.onmicrosoft.com>
2025-07-22 10:29:20 -0400

    subnets vars1 (#56)
    * subnets vars1
    
    * close brackets

--- modules/vnets/variables.tf
+++ modules/vnets/variables.tf
@@ -43,6 +43,53 @@ variable "subnets" {
     address_prefix   = optional(string)
     address_prefixes = optional(list(string))
     name             = string
+    nat_gateway = optional(object({
+      id = string
+    }))
+    network_security_group = optional(object({
+      id = string
+    }))
+    private_endpoint_network_policies             = optional(string, "Enabled")
+    private_link_service_network_policies_enabled = optional(bool, true)
+    route_table = optional(object({
+      id = string
+    }))
+    service_endpoint_policies = optional(map(object({
+      id = string
+    })))
+    service_endpoints               = optional(set(string))
+    default_outbound_access_enabled = optional(bool, false)
+    sharing_scope                   = optional(string, null)
+    delegation = optional(list(object({
+      name = string
+      service_delegation = object({
+        name = string
+      })
+    })))
+    timeouts = optional(object({
+      create = optional(string, "30m")
+      read   = optional(string, "5m")
+      update = optional(string, "30m")
+      delete = optional(string, "30m")
+    }), {})
+    retry = optional(object({
+      error_message_regex  = optional(list(string), ["ReferencedResourceNotProvisioned"])
+      interval_seconds     = optional(number, 10)
+      max_interval_seconds = optional(number, 180)
+      multiplier           = optional(number, 1.5)
+      randomization_factor = optional(number, 0.5)
+    }), {})
+    role_assignments = optional(map(object({
+      role_definition_id_or_name             = string
+      principal_id                           = string
+      description                            = optional(string, null)
+      skip_service_principal_aad_check       = optional(bool, false)
+      condition                              = optional(string, null)
+      condition_version                      = optional(string, null)
+      delegated_managed_identity_resource_id = optional(string, null)
+      principal_type                         = optional(string, null)
+    })))
   }))
-  default = {}
+  default     = {}
+  description = "All subnet vars"
 }
