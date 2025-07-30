b715d5e77a14bcb5c836a5b9c357185137c1b09d
CptNoodlz <techadminoo@63q8gc.onmicrosoft.com>
2025-07-30 16:12:49 -0400

    commiteroni1 (#58)
    Co-authored-by: Ozan <463721@wegmans.com>

---
+++ rdc/vnets/vnet-test-2-lol-cool.tf
@@ -0,0 +1,21 @@
+module "this" {
+  source = "../../modules/vnets/"
+
+  address_space       = ["10.0.0.0/16"]
+  location            = "East US"
+  name                = "TestVNet"
+  resource_group_name = "Testrg1"
+  dns_servers = {
+    dns_servers = ["8.8.8.8"]
+  }
+  subnets = {
+    "subnet1" = {
+      name             = "subnet1"
+      address_prefixes = ["10.25.0.0/19"]
+    }
+    "subnet2" = {
+      name             = "subnet2"
+      address_prefixes = ["10.35.0.0/19"]
+    }
+  }
+}
