f159d4281bf5124f440e784c23dce9172329a60a
Ozan <463721@wegmans.com>
2025-08-01 09:58:24 -0400

    anoootha 1

--- rdc/vnets/vnet-test-2-lol-cool.tf
+++ rdc/vnets/vnet-test-2-lol-cool.tf
@@ -19,3 +19,25 @@ module "this" {
     }
   }
 }
+
+module "that" {
+  source = "../../modules/vnets/"
+
+  address_space       = ["28.0.0.0/16"]
+  location            = "East US"
+  name                = "TestVNet2"
+  resource_group_name = "Testrg1"
+  dns_servers = {
+    dns_servers = ["8.8.8.8"]
+  }
+  subnets = {
+    "subnet1" = {
+      name             = "subnet1"
+      address_prefixes = ["28.25.0.0/19"]
+    }
+    "subnet2" = {
+      name             = "subnet2"
+      address_prefixes = ["28.35.0.0/19"]
+    }
+  }
+}
