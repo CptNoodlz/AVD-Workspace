8959aa93cc0cda4b723ee6c071c9b67fb9a9611a
Ozan <463721@wegmans.com>
2025-10-21 15:36:43 -0400

    change module definitions

--- modules/vnets/main.tf
+++ modules/vnets/main.tf
@@ -6,8 +6,8 @@ module "vnets" {
   name                = var.name
   address_space       = var.address_space
   dns_servers         = var.dns_servers
-  delegation          = var.subnets
-  private_endpoint_network_policies = var.subnets
-  private_link_service_network_policies = var.subnets
+  delegation          = var.subnetsconfig
+  private_endpoint_network_policies = var.subnetsconfig
+  private_link_service_network_policies = var.subnetsconfig
    
 }
