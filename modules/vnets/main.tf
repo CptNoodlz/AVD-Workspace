b4a61c9f828e0cf35d2cbb009d70a636d5f9519e
CptNoodlz <techadminoo@63q8gc.onmicrosoft.com>
2025-12-12 10:26:45 -0500

    Update main.tf (#68)

--- modules/vnets/main.tf
+++ modules/vnets/main.tf
@@ -1,7 +1,7 @@
 
 module "vnets" {
   source  = "Azure/avm-res-network-virtualnetwork/azurerm"
-  version = "0.8.1"
+  version = "0.16.0"
 
   resource_group_name = var.resource_group_name
   location            = var.location
