5f5147da64a7c0ffb464918572f4ed0b3c7c079f
CptNoodlz <techadminoo@63q8gc.onmicrosoft.com>
2025-12-22 14:56:53 -0500

    change locals

--- modules/vnets/main.tf
+++ modules/vnets/main.tf
@@ -22,33 +22,34 @@ module "vnets" {
 locals {
   normalized_subnets = {
     for sn_name, sn in var.subnets : sn_name => {
+      # AVM requires a "name" attribute for each subnet object
+      name = coalesce(
+        try(sn.name, null),
+        sn_name
+      )
+
       # Always produce a list (AVM expects address_prefixes list)
       address_prefixes = coalesce(
         sn.address_prefixes,
         sn.address_prefix != null ? [sn.address_prefix] : []
       )
 
-      # Only build nested objects when IDs provided
       nat_gateway            = sn.nat_gateway_id != null ? { id = sn.nat_gateway_id } : null
       network_security_group = sn.network_security_group_id != null ? { id = sn.network_security_group_id } : null
       route_table            = sn.route_table_id != null ? { id = sn.route_table_id } : null
 
-      # Service endpoints & policies passthrough
       service_endpoints = sn.service_endpoints
       service_endpoint_policies = sn.service_endpoint_policy_ids != null ? {
         for i, sep_id in sn.service_endpoint_policy_ids : "policy_${i}" => { id = sep_id }
       } : null
 
-      # Policy flags (AVM expects string Enabled/Disabled for private endpoint policies)
       private_endpoint_network_policies             = sn.private_endpoint_network_policies_enabled ? "Enabled" : "Disabled"
       private_link_service_network_policies_enabled = sn.private_link_service_network_policies_enabled
 
-      # Delegation transformed to list (older AVM schema uses list of objects)
       delegation = sn.delegation != null ? [{
         name = sn.delegation.name
         service_delegation = {
           name = sn.delegation.service_delegation.name
-          # actions not required in virtual network inline subnet for basic scenario; omit for simplicity
         }
       }] : null
     }
