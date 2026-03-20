59aea78feaab85024718f61d8a6799490f7d2393
Ozan <463721@wegmans.com>
2026-03-20 13:46:47 -0400

    scaffold: add modules/aks, networking, identity, storage + envs/dev, prod + pipelines2

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -14,24 +14,24 @@ module "aks" {
   location            = var.location
   resource_group_name = var.resource_group_name
 
-  kubernetes_version        = var.kubernetes_version
-  sku_tier                  = var.sku_tier
-  private_cluster_enabled   = var.private_cluster_enabled
+  kubernetes_version      = var.kubernetes_version
+  sku_tier                = var.sku_tier
+  private_cluster_enabled = var.private_cluster_enabled
 
   default_node_pool = {
-    name       = "system"
-    vm_size    = var.default_node_pool_vm_size
-    node_count = var.default_node_pool_node_count
+    name            = "system"
+    vm_size         = var.default_node_pool_vm_size
+    node_count      = var.default_node_pool_node_count
     os_disk_size_gb = var.default_node_pool_os_disk_size_gb
     vnet_subnet_id  = var.default_node_pool_subnet_id
   }
 
   network_profile = {
-    network_plugin     = var.network_plugin
-    network_policy     = var.network_policy
-    dns_service_ip     = var.dns_service_ip
-    service_cidr       = var.service_cidr
-    load_balancer_sku  = "standard"
+    network_plugin    = var.network_plugin
+    network_policy    = var.network_policy
+    dns_service_ip    = var.dns_service_ip
+    service_cidr      = var.service_cidr
+    load_balancer_sku = "standard"
   }
 
   identity = {
